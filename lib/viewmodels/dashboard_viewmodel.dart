3842a772e94c51daea950bd95938b6eb3bfcfd0c
naveenyadav15 <naveenyadav4116@gmail.com>
2025-02-13 22:27:55 +0530

    updated

--- lib/viewmodels/dashboard_viewmodel.dart
+++ lib/viewmodels/dashboard_viewmodel.dart
@@ -4,7 +4,7 @@ import '../services/dashboard_service.dart';
 
 class DashboardViewModel extends ChangeNotifier {
   final DashboardService _service = DashboardService();
-  
+
   List<Visitor> _visitors = [];
   List<Visitor> get visitors => _visitors;
 
